9d212cbe8d2dd14369c12b3573c9ee7c040625d1
kikiyume280 <yourenrigang@gmail.com>
2025-12-10 19:26:47 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
-// ここに自分の Firebase 設定を書き換えてください
+// Firebase（script タグ版）
 import { initializeApp } 
   from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getFirestore } 
+import { getFirestore }
   from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR.firebaseapp.com",
-  projectId: "YOUR",
-  storageBucket: "YOUR.appspot.com",
-  messagingSenderId: "YOUR",
-  appId: "YOUR"
+  apiKey: "AIzaSyCC7KAv7V4j1Z6-o1Y8ikmb3r5htN9O_aA",
+  authDomain: "zzgohan-280.firebaseapp.com",
+  projectId: "zzgohan-280",
+  storageBucket: "zzgohan-280.firebasestorage.app",
+  messagingSenderId: "459336048542",
+  appId: "1:459336048542:web:ff6c825dec81cc7df8008f",
+  measurementId: "G-JX4RP9LWR3"
 };
 
+// Firebase 初期化
 export const app = initializeApp(firebaseConfig);
+
+// Firestore 初期化
 export const db = getFirestore(app);
