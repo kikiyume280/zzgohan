151792d34de42450af83a6de3ee1ca77b4a29658
kikiyume280 <yourenrigang@gmail.com>
2025-12-10 20:10:53 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,7 +13,13 @@ appId: "1:...:web:..."
 
 
 const firebaseConfig = {
-// <-- ここに貼る
+  apiKey: "AIzaSyCC7KAv7V4j1Z6-o1Y8ikmb3r5htN9O_aA",
+  authDomain: "zzgohan-280.firebaseapp.com",
+  projectId: "zzgohan-280",
+  storageBucket: "zzgohan-280.firebasestorage.app",
+  messagingSenderId: "459336048542",
+  appId: "1:459336048542:web:ff6c825dec81cc7df8008f",
+  measurementId: "G-JX4RP9LWR3"
 };
 
 
